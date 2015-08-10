645267681159f1ac57949a97ee09235277e8bbcc
Yago Elias <yagothemaster2@gmail.com>
2015-08-10 20:32:40 -0300

    algumas instruções

---
+++ LPD.VirtualMachine/LPD.VirtualMachine/Engine/Instructions/DIVInstruction.cs
@@ -0,0 +1,18 @@
+﻿namespace LPD.VirtualMachine.Engine.Instructions
+{
+    [Instruction(DIV)]
+    class DIVInstruction : IncrementalInstruction
+    {
+        protected override void SpecificExecute(ExecutionContext context, string[] parameters)
+        {
+            int first;
+            int second;
+            Stack stack = context.Memory.StackRegion;
+
+            first = stack.Load();
+            stack.Down();
+            second = stack.Load();
+            stack.Store(first / second);
+        }
+    }
+}
---
+++ LPD.VirtualMachine/LPD.VirtualMachine/Engine/Instructions/SUBInstruction.cs
@@ -0,0 +1,23 @@
+﻿namespace LPD.VirtualMachine.Engine.Instructions
+{
+    [Instruction(SUB)]
+    class SUBInstruction: IncrementalInstruction
+    {
+        /// <summary>
+        /// Executes the ADD instruction.
+        /// </summary>
+        /// <param name="context">The execution context.</param>
+        /// <param name="parameters">Not used.</param>
+        protected override void SpecificExecute(ExecutionContext context, string[] parameters)
+        {
+            int first;
+            int second;
+            Stack stack = context.Memory.StackRegion;
+
+            first = stack.Load();
+            stack.Down();
+            second = stack.Load();
+            stack.Store(first - second);
+        }
+    }
+}
