1b8e75b4258ad43969782205e09243c02051f1ec
Yago Elias <yagothemaster2@gmail.com>
2015-08-11 20:56:57 -0300

    d

---
+++ LPD.VirtualMachine/LPD.VirtualMachine/Engine/Instructions/CEQInstruction.cs
@@ -0,0 +1,28 @@
+﻿using static LPD.VirtualMachine.Engine.InstructionSet;
+
+namespace LPD.VirtualMachine.Engine.Instructions
+{
+    [Instruction(CEQ)]
+    class CEQInstruction : IncrementalInstruction
+    {
+        protected override void SpecificExecute(ExecutionContext context, string[] parameters)
+        {
+            int first;
+            int second;
+            Stack stack = context.Memory.StackRegion;
+
+            first = stack.Load();
+            stack.Down();
+            second = stack.Load();
+            if (second == first)
+            {
+                second = 1;
+            }
+            else
+            {
+                second = 0;
+            }
+            stack.Store(second);
+        }
+    }
+}
---
+++ LPD.VirtualMachine/LPD.VirtualMachine/Engine/Instructions/CMEInstruction.cs
@@ -0,0 +1,30 @@
+﻿using static LPD.VirtualMachine.Engine.InstructionSet;
+
+namespace LPD.VirtualMachine.Engine.Instructions
+{
+    [Instruction(CME)]
+    class CMEInstruction: IncrementalInstruction
+    {
+        protected override void SpecificExecute(ExecutionContext context, string[] parameters)
+        {
+            int first;
+            int second;
+            Stack stack = context.Memory.StackRegion;
+
+            first = stack.Load();
+            stack.Down();
+            second = stack.Load();
+
+            if (second < first)
+            {
+                second = 1;
+            }
+            else
+            {
+                second = 0;
+            }
+
+            stack.Store(second);
+        }
+    }
+}
