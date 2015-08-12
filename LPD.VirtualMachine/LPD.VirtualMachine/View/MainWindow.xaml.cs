f6f51a98acb2d77cd7ef77b351bdbaac3be3bee2
André Nicoletti <andre_nls@live.com>
2015-08-11 23:01:40 -0300

    Added drag and drop to load file.

--- LPD.VirtualMachine/LPD.VirtualMachine/View/MainWindow.xaml.cs
+++ LPD.VirtualMachine/LPD.VirtualMachine/View/MainWindow.xaml.cs
@@ -4,9 +4,12 @@ using LPD.VirtualMachine.Properties;
 using MahApps.Metro.Controls;
 using Microsoft.Win32;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using static System.IO.Path;
+using System;
 
 namespace LPD.VirtualMachine.View
 {
@@ -19,6 +22,7 @@ namespace LPD.VirtualMachine.View
 
         private string _selectedFilePath;
         private Window _executionWindow;
+        private bool _isStartButtonVisible = false;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
@@ -41,8 +45,22 @@ namespace LPD.VirtualMachine.View
                 StartButton.Margin = new Thickness(50, 13, 0, 0);
                 StartButton.Width = 250d;
                 sb.Stop();
+                _isStartButtonVisible = true;
             };
             sb.Begin();
+        }
+
+        private void StopDragEnterAnimation()
+        {
+            Storyboard sb = FindResource("DragEndAnimation") as Storyboard;
+
+            sb.Completed += delegate
+            {
+                LoadButton.RenderTransform = new ScaleTransform();
+                sb.Stop(this);
+            };
+            (FindResource("DragBeginAnimation") as Storyboard).Stop(this);
+            sb.Begin(this, true);
         }
 
         /// <summary>
@@ -50,7 +68,7 @@ namespace LPD.VirtualMachine.View
         /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The info of the event.</param>
-        private async void OnLoadButtonClick(object sender, RoutedEventArgs e)
+        private void OnLoadButtonClick(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog()
             {
@@ -61,13 +79,19 @@ namespace LPD.VirtualMachine.View
 
             if (!result.HasValue || !result.Value)
             {
-                return; 
+                return;
             }
 
+            _selectedFilePath = openFileDialog.FileName;
+            PrepareExecution();
+        }
+
+        private async void PrepareExecution()
+        {
             _executionWindow = new ExecutionWindow(GetFileNameWithoutExtension(_selectedFilePath));
 
             int virtualMachineSize = (int)Settings.Default[App.SystemMemorySettingKey];
-            InstructionSet instructionsCollection = await InstructionSet.CreateFromFileAsync(openFileDialog.FileName);
+            InstructionSet instructionsCollection = await InstructionSet.CreateFromFileAsync(_selectedFilePath);
             Memory memory = Memory.CreateMemory(virtualMachineSize, instructionsCollection);
             ExecutionContext context = new ExecutionContext()
             {
@@ -78,8 +102,11 @@ namespace LPD.VirtualMachine.View
             };
 
             CPU.Instance.Initialize(context);
-            DoStartButtonAnimation();
-            _selectedFilePath = openFileDialog.FileName;
+
+            if (!_isStartButtonVisible)
+            {
+                DoStartButtonAnimation();
+            }
         }
 
         /// <summary>
@@ -100,6 +127,39 @@ namespace LPD.VirtualMachine.View
         private void OnSettingsButtonClick(object sender, RoutedEventArgs e)
         {
             new SettingsWindow().ShowDialog();
+        }
+
+        private void OnLoadButtonDrop(object sender, DragEventArgs e)
+        {
+            StopDragEnterAnimation();
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string file = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+
+                _selectedFilePath = file;
+                PrepareExecution();
+            }
+        }
+
+        private void OnLoadButtonDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.OriginalSource.GetType() != typeof(Image))
+            {
+                return;
+            }
+
+            (FindResource("DragBeginAnimation") as Storyboard).Begin(this, true);
+        }
+
+        private void OnLoadButtonDragLeave(object sender, DragEventArgs e)
+        {
+            if (e.OriginalSource.GetType() != typeof(Image))
+            {
+                return;
+            }
+
+            StopDragEnterAnimation();
         }
     }
 }
